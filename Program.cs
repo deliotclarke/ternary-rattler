0c9f034e9f56175ff4e18370558d33b6372a9609
Daniel Clarke <deliotclarke@gmail.com>
2019-07-14 17:42:48 -0500

    creation commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TernaryRattler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
