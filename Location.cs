a46c1d1af0be869474668dbc4961b9842bd2fef1
Daniel Clarke <deliotclarke@gmail.com>
2019-07-14 19:02:52 -0500

    created repository. added some programmy stuff. working on adding a location - got the logic written out.

---
+++ Location.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace TernaryRattler
+{
+    class Location
+    {
+        public string ParkName { get; set; }
+        public string State { get; set; }
+        public bool Visited { get; set; }
+        public bool Rattled { get; set; }
+
+        public void EditMe()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,51 @@ namespace TernaryRattler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+            Console.WriteLine("Hello World Traveller!");
+            Console.WriteLine("Welcome to Ternary Rattler. What do you want to do?");
+            Console.WriteLine();
+            Console.WriteLine("UPDATE your travel list, ADD to your travel list, VIEW your travel list?");
+            string userStart = Console.ReadLine();
+
+            if (userStart.ToUpper() == "UPDATE")
+            {
+                Console.WriteLine("Update");
+            }
+            else if (userStart.ToUpper() == "ADD")
+            {
+                AddLocation.AddNewLocation();
+            }
+            else if (userStart.ToUpper() == "VIEW")
+            {
+                Console.WriteLine("View");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Your answer sucks.");
+                Console.WriteLine();
+                Console.WriteLine("End program?");
+                string userEnd = Console.ReadLine();
+
+                if (userEnd.ToUpper() == "Y")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Okaayyyyy byyyyyeeeeeee");
+                }
+                else if (userEnd.ToUpper() == "N")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Okay, let's start over.");
+                    //return to original user input
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Your answer sucks again. BUH!");
+                }
+            }
         }
     }
 }
